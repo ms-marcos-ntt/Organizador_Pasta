8fa7458ecbe36f03efb05d397706bb4eb9ea99b2
Marcos Santos - Net Turbo <50344569+marcos-rts@users.noreply.github.com>
2025-05-21 11:57:35 -0300

    adiciona a estrutura inicial do aplicativo organizador com funcionalidades de janela e menu

---
+++ organizer/main.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow, Menu, Tray, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const monitorarDownloads = require('./monitor');
+
+let tray = null;
+let win = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    icon: path.join(__dirname, 'icons', 'icon.png'),
+  });
+
+  win.loadFile('index.html');
+}
+
+function criarMenu() {
+  tray = new Tray(path.join(__dirname, 'icons', 'icon.png'));
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Configurar Bot Telegram',
+      click: () => {
+        win.show();
+      },
+    },
+    {
+      label: 'Sair',
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip('Organizador de Downloads');
+  tray.setContextMenu(menu);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  criarMenu();
+  monitorarDownloads();
+  win.hide();
+});
+
+app.on('window-all-closed', (e) => e.preventDefault());
